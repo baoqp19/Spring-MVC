6e534fcdbbd3db0d5aa7dc72a118885c915856c2
= <=>
2024-08-17 22:22:08 +0700

    View engine

--- src/main/java/vn/hoidanit/laptopshop/controllor/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controllor/UserController.java
@@ -2,21 +2,28 @@ package vn.hoidanit.laptopshop.controllor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.Module;
+
 import vn.hoidanit.laptopshop.service.UserService;
+
+
 
 @Controller
 public class UserController {
-    private final UserService userService;
+    private UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService){
         this.userService = userService;
     }
 
     @RequestMapping("/")
-    public String gethomo() {
-        return "hello.html";
+    public String gethomo(Model model) {
+        String test = this.userService.HandleHello();
+        model.addAttribute("bao", test);
+        return "helle";
     }
 }
