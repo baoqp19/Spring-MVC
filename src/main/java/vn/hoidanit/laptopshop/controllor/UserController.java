afa523c6b97bc11883580053a6206a173116598d
= <=>
2024-08-18 09:05:50 +0700

    Bootstrap add user

--- src/main/java/vn/hoidanit/laptopshop/controllor/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controllor/UserController.java
@@ -16,14 +16,39 @@ import vn.hoidanit.laptopshop.service.UserService;
 public class UserController {
     private UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
-    public String gethomo(Model model) {
-        String test = this.userService.HandleHello();
-        model.addAttribute("bao", test);
-        return "helle";
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with model");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with model");
+        return "admin/user/create";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// // DI : dependency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -14,15 +14,11 @@
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+            <link rel="stylesheet" href="/css/demo.css">
         </head>
 
         <body>
-            hello from jsp
-
-            <h1>
-                ${bao}
-            </h1>
-            <button class="btn btn-primary">Submit</button>
-        </body>
+         
+              
 
         </html>
