a8b20ff1024c4bbdd84a0decc68c5e00ab325a2b
= <=>
2024-08-18 10:22:46 +0700

    Save user into database

--- src/main/java/vn/hoidanit/laptopshop/controllor/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controllor/UserController.java
@@ -13,34 +13,42 @@ import com.fasterxml.jackson.databind.Module;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
-    private UserService userService;
 
-    public UserController(UserService userService) {
+    private final UserService userService;
+
+    public UserController(
+            UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("eric", "test");
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
-        String test = this.userService.handleHello();
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {  // lấy ra giá trị kiểu User
-        System.out.println("run here: " + user);
-       return "hello";
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println(" run here " + hoidanit);
+        this.userService.handleSaveUser(hoidanit);
+        return "hello";
     }
 }
